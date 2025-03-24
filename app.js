9223201ad87ad5abb55b41a40d1539a192314ebc
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-03-24 22:52:18 +0100

    feat: remove Stripe integration and related controllers for cleanup

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-require('dotenv').config();
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
 
+// importa cors
 const cors = require('cors');
-const stripeRouter = require('./routers/stripe');
+
+// importa router products
 const productsRouter = require('./routers/products');
+
+// importa middlewares
 const notFound = require('./middlewares/errorHandler');
 const handleErrors = require('./middlewares/errorHandler');
 const imagePath = require('./middlewares/imagePath');
 
-// ⚠️ AGGIUNGI QUESTO:
-const { handleStripeWebhook } = require('./controllers/stripeWebHookController');
+
 
 app.use(cors({ origin: process.env.FE_APP }));
 app.use(imagePath);
 app.use(express.static('public'));
 
-// ✅ CORRETTO: webhook con raw + handler diretto
-app.post(
-    '/api/stripe/webhook',
-    express.raw({ type: 'application/json' }),
-    handleStripeWebhook
-);
+
 
 // Parser normale
 app.use(express.json());
@@ -31,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Altre rotte
 app.use('/api/products', productsRouter);
-app.use('/api/stripe', stripeRouter);
+
 
 app.get('/api', (req, res) => {
     res.send("Ciao sono la rotta Home");
